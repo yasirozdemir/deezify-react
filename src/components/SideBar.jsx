1690d3b53585a9a0fd3f4db041d16a8e56c07652
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-17 19:05:28 +0300

    sidebar update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -84,7 +84,7 @@ const SideBar = () => {
       <div className="navigation d-flex">
         <ul>
           <li>
-            <Link to="/">
+            <Link id="createPlaylistButton" to="/">
               <svg role="img" height="12" width="12" viewBox="0 0 16 16">
                 <path d="M15.25 8a.75.75 0 01-.75.75H8.75v5.75a.75.75 0 01-1.5 0V8.75H1.5a.75.75 0 010-1.5h5.75V1.5a.75.75 0 011.5 0v5.75h5.75a.75.75 0 01.75.75z"></path>
               </svg>
@@ -92,7 +92,7 @@ const SideBar = () => {
             </Link>
           </li>
           <li>
-            <Link to="/">
+            <Link id="likedSongsLink" to="/">
               <svg role="img" height="12" width="12" viewBox="0 0 16 16">
                 <path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z"></path>
               </svg>
