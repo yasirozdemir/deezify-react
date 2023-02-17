65367d50366e3e51291727c301d7042d6ce89439
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-17 18:38:01 +0300

    player icons update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,8 +11,6 @@ const Player = () => {
   const likedSongs = useSelector((state) => state.lists.likedSongs);
   const isLiked = likedSongs.some((s) => selectedSong.id === s.id);
 
-  console.log(selectedSong);
-
   return (
     <footer className="fixed-bottom d-flex align-items-center justify-content-between">
       <div className="d-flex footer-info align-items-center w-30">
@@ -78,7 +76,29 @@ const Player = () => {
           style={{ backgroundColor: "transparent" }}
         />
       </div>
-      <div className="d-flex justify-content-end w-30">helo</div>
+      <div className="d-flex justify-content-end align-items-center footer-controls w-30">
+        <button className="btn-transparent px-2">
+          <svg role="img" height="16" width="16" aria-hidden="true">
+            <path d="M13.426 2.574a2.831 2.831 0 00-4.797 1.55l3.247 3.247a2.831 2.831 0 001.55-4.797zM10.5 8.118l-2.619-2.62A63303.13 63303.13 0 004.74 9.075L2.065 12.12a1.287 1.287 0 001.816 1.816l3.06-2.688 3.56-3.129zM7.12 4.094a4.331 4.331 0 114.786 4.786l-3.974 3.493-3.06 2.689a2.787 2.787 0 01-3.933-3.933l2.676-3.045 3.505-3.99z"></path>
+          </svg>
+        </button>
+        <button className="btn-transparent px-2">
+          <svg role="img" height="16" width="16" aria-hidden="true">
+            <path d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 013.5 1h9a2.5 2.5 0 010 5h-9A2.5 2.5 0 011 3.5zm2.5-1a1 1 0 000 2h9a1 1 0 100-2h-9z"></path>
+          </svg>
+        </button>
+        <button className="btn-transparent px-2">
+          <svg role="presentation" height="16" width="16" aria-hidden="true">
+            <path d="M6 2.75C6 1.784 6.784 1 7.75 1h6.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0114.25 15h-6.5A1.75 1.75 0 016 13.25V2.75zm1.75-.25a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h6.5a.25.25 0 00.25-.25V2.75a.25.25 0 00-.25-.25h-6.5zm-6 0a.25.25 0 00-.25.25v6.5c0 .138.112.25.25.25H4V11H1.75A1.75 1.75 0 010 9.25v-6.5C0 1.784.784 1 1.75 1H4v1.5H1.75zM4 15H2v-1.5h2V15z"></path>
+            <path d="M13 10a2 2 0 11-4 0 2 2 0 014 0zm-1-5a1 1 0 11-2 0 1 1 0 012 0z"></path>
+          </svg>
+        </button>
+        <button className="btn-transparent">
+          <svg role="img" height="16" width="16" aria-hidden="true">
+            <path d="M6.53 9.47a.75.75 0 010 1.06l-2.72 2.72h1.018a.75.75 0 010 1.5H1.25v-3.579a.75.75 0 011.5 0v1.018l2.72-2.72a.75.75 0 011.06 0zm2.94-2.94a.75.75 0 010-1.06l2.72-2.72h-1.018a.75.75 0 110-1.5h3.578v3.579a.75.75 0 01-1.5 0V3.81l-2.72 2.72a.75.75 0 01-1.06 0z"></path>
+          </svg>
+        </button>
+      </div>
     </footer>
   );
 };
